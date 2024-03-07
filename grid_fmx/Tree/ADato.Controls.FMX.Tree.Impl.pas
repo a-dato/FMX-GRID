b16be169da455555fe3568a0fbadf9cdb67940c9
Alex <alexander.shyshko@a-dato.net>
2024-03-08 00:31:16 +0800

    to previous

--- grid_fmx/Tree/ADato.Controls.FMX.Tree.Impl.pas
+++ grid_fmx/Tree/ADato.Controls.FMX.Tree.Impl.pas
@@ -5586,7 +5586,7 @@ end;
 procedure TCustomTreeControl.ResetView(const Full: Boolean = True; const SaveTopRow: Boolean = False;
   const ATopRowMinHeight: Single = 0);
 begin
-  if SaveTopRow and (_View <> nil) then
+  if not Full and (_View <> nil) then
     _currentPosition := View.Current;
 
   inherited;  // this will View := nil if Full = True
